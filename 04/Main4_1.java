ab5a59feb97108f71c979c173d25af65657c9bda
hiro0689 <95416183+hiro0689@users.noreply.github.com>
2021-12-04 00:51:39 +0900

    20211204newjava

---
+++ 04/Main4_1.java
@@ -0,0 +1,9 @@
+public class Main4_1java{
+ public static void main(String[] args){
+
+ int    [] points = new int[4];
+double  [] weight = new double[5];
+boolean [] answers = new boolean[3];
+String  [] names = new String[3];
+}
+ }
