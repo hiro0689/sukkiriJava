6d1013c10076a83fcbe576257caacf78ea790365
funatti055 <52117097+funatti055@users.noreply.github.com>
2021-12-09 22:00:17 +0900

    20211209_reviewByFunatsu

--- 04/Main4_1.java
+++ 04/Main4_1.java
@@ -1,9 +1,8 @@
-public class Main4_1java{
- public static void main(String[] args){
-
- int    [] points = new int[4];
-double  [] weight = new double[5];
-boolean [] answers = new boolean[3];
-String  [] names = new String[3];
+public class Main4_1java{  //[船津]ブロック単位で、字下げを4文字間隔で行うこと
+    public static void main(String[] args){ //[船津]配列のデータ型は[]をくっつける
+        int[] points = new int[4];
+        double[] weight = new double[5];
+        boolean[] answers = new boolean[3];
+        String[] names = new String[3];
+    }
 }
- }
--- 04/Main4_2.java
+++ 04/Main4_2.java
@@ -1,4 +1,4 @@
-public class Main4_2 {
+public class Main4_2 {  //[船津]ブロック単位で、字下げを4文字間隔で行うこと
   public static void main(String[] args){
    int[] moneyList = { 121902,8302,55100};
    for (int i = 0; i < moneyList.length; i++) {
