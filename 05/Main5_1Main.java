8f5e302712760e495b3828adc3acb25b4ebbd4ea
hiro0689 <95416183+hiro0689@users.noreply.github.com>
2021-12-09 21:25:17 +0900

    20211209_new

---
+++ 05/Main5_1Main.java
@@ -0,0 +1,18 @@
+import java.time.zone.ZoneOffsetTransition;
+
+public class Main5_1Main {
+  public static void main(String[] args){
+    introduceOneself();     
+  }  
+  
+  public static void introduceOneself(){
+    String name = "中森　大雅";
+    int age = 19;
+    int height = 168;
+    char eto = "ウサギ";
+    System.out.println("私の名前は" + name + "です");
+    System.out.println("歳は" + age + "です" );
+    System.out.println("身長は" + height + "cmです");
+    System.out.println("十二干支は" + eto + "です");  
+  }
+}
