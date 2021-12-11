7dea822df657f135d8ab6a649c986d451cb94bd6
hiro0689 <95416183+hiro0689@users.noreply.github.com>
2021-12-11 22:13:50 +0900

    Main5_2Main.javanew

---
+++ Main5_2Main.java
@@ -0,0 +1,13 @@
+public class Main5_2Main {
+ public static void main(string[] args){
+        String title ="お誘い";
+        String address ="usc800@xxxx.com";
+        email(title.address,text);
+ }
+    public static void email(String title, String address,String text){
+       System.out.println
+          (address + "に,以下のメールを送信しました。");
+       System.out.println("件名：無題");
+       System.out.println("本文：" + text);
+    }
+}
